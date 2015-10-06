1270f8caf471b6cad29f775ce0329ba4f5b554dd
dom <contact@dominicmusgrave.co.uk>
2015-10-06 22:47:38 +0100

    [DESIGN] Added images and modded the design of the site
    Created some image assets to use in the footer. Give the site a bit of a better look than just programmer-style coloured-boxes.
    Also updated the scss accordingly with a new background colour and related stylings
    for the new assets.

---
+++ public/scripts/recipe_controller.js
@@ -0,0 +1,3 @@
+function OnAddIngrediantClick() {
+  ingrediants += 1;
+}
--- routes/create.js
+++ routes/create.js
@@ -4,20 +4,9 @@ var router = express.Router();
 var mongodb = require('mongodb');
 var recipeModel = require('../models/recipe');
 
-var defaultContent = function() {
-  recipeModel.addIngrediant("Empty", 10.0);
-  console.log(recipeModel.getIngrediants());
-  recipeModel.setTitle("Empty");
-
-  return {
-    title: "Recipes",
-    recipes: JSON.stringify(recipeModel.getIngrediants())
-  };
-};
-
 /* GET home page. */
 router.get('/recipe', function(req, res, next) {
-  res.render('create/recipe', defaultContent());
+  res.render('create/recipe', {ingrediants: [true]});
 });
 
 module.exports = router;
