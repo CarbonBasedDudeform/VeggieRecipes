c89b9e61930de25d1a9a1a126332e1bf9a0416c7
Dominic Musgrave <contact@dominicmusgrave.co.uk>
2019-05-04 00:11:06 +0100

    Refactored the default recipe to be part of recipe.js

--- models/recipe.js
+++ models/recipe.js
@@ -16,3 +16,18 @@ Recipe.prototype.getTitle = function() { return this.title; };
 Recipe.prototype.setTitle = function(newTitle) { this.title = newTitle; };
 
 module.exports = Recipe;
+
+var defaultContent = function() {
+  var rec = new Recipe('Create Your Own Recipes And See Them Here');
+  rec.addIngrediant("Creativity", 1.0);
+  rec.addIngrediant("Taste", 2.0);
+  rec.addIngrediant("Skill", 1.0);
+  rec.addIngrediant("Desire for delicious food", 10.0);
+
+  var recipesArray = [];
+  recipesArray.push(rec);
+
+  return {title: "Your Recipes", recipes: recipesArray};
+};
+
+module.exports.defaultRecipe = defaultContent;
--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,9 @@ var mongodb = require('mongodb');
 //custom models
 var recipeModel = require('../models/recipe');
 
-var defaultContent = function() {
-  var rec = new recipeModel('Create Your Own Recipes And See Them Here');
-  rec.addIngrediant("Creativity", 1.0);
-  rec.addIngrediant("Taste", 2.0);
-  rec.addIngrediant("Skill", 1.0);
-  rec.addIngrediant("Desire for delicious food", 10.0);
-
-  var recipesArray = [];
-  recipesArray.push(rec);
-
-  return {title: "Your Recipes", recipes: recipesArray};
-};
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', defaultContent());
+  res.render('index', recipeModel.defaultRecipe());
 });
 
 router.get('/error', function(req, res, next) {
